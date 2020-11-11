5a969844d6913f94ef2e02580097a840d32676b2
Nicolas Borboën <ponsfrilus@gmail.com>
2020-11-11 09:43:21 +0100

    [version] bump to v0.0.5

--- epfl-coming-soon.php
+++ epfl-coming-soon.php
@@ -4,14 +4,14 @@ Plugin Name: EPFL Coming Soon
 Plugin URI: https://github.com/epfl-si/wp-plugin-epfl-coming-soon
 Description: EPFL coming soon / maintenance plugin
 Author: EPFL SI
-Version: 0.0.4
+Version: 0.0.5
 Author URI: https://github.com/epfl-si
 */
 
 defined('ABSPATH')
     or die('Direct access not allowed.');
 
-define("EPFL_COMING_SOON_VERSION", "0.0.4");
+define("EPFL_COMING_SOON_VERSION", "0.0.5");
 
 require_once WP_CONTENT_DIR . '/plugins/epfl-coming-soon/src/classes/epfl-coming-soon.php';
 
